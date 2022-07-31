0050a5e8022579633981acc4d6587f72741cf244
Susbot <susbot651968498@gmail.com>
2022-07-31 17:12:07 -0600

    API View Class

--- apis/root/urls.py
+++ apis/root/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from apis.root.views import article_list_create_api_view, article_detail_api_view
+from apis.root.views import ArticleListCreateAPIView, ArticleDetailAPIView
+# from apis.root.views import article_list_create_api_view, article_detail_api_view
 
 
 urlpatterns = [
-    path("articles/", article_list_create_api_view, name="article-list"),
-    path("articles/<int:pk>/", article_detail_api_view, name="article-detail"),
+    path("articles/", ArticleListCreateAPIView.as_view(), name="article-list"),
+    path("articles/<int:pk>/", ArticleDetailAPIView.as_view(), name="article-detail"),
+
+    # path("articles/", article_list_create_api_view, name="article-list"),
+    # path("articles/<int:pk>/", article_detail_api_view, name="article-detail"),
 ]
