27a43f75d2ec66d0b340d5ad6e88cc9b1fc94d0b
Susbot <susbot651968498@gmail.com>
2022-07-31 16:45:25 -0600

    API View Dectorator

--- apis/root/urls.py
+++ apis/root/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from apis.root.views import article_list_create_api_view
+from apis.root.views import article_list_create_api_view, article_detail_api_view
 
 
 urlpatterns = [
-    path("articles/", article_list_create_api_view, name="article-list")
+    path("articles/", article_list_create_api_view, name="article-list"),
+    path("articles/<int:pk>/", article_detail_api_view, name="article-detail"),
 ]
